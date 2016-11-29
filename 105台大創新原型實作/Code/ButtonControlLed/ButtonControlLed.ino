b9a08cab45a00498fa843cb19293b49a9a16fe50
Bruce Tsao <prgbruce@gmail.com>
2016-11-29 13:01:24 +0800

    commit all

--- 105台大創新原型實作/Code/ButtonControlLed/ButtonControlLed.ino
+++ 105台大創新原型實作/Code/ButtonControlLed/ButtonControlLed.ino
@@ -1,19 +1,3 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
-
 // the setup function runs once when you press reset or power the board
 
 void setup() {
@@ -25,13 +9,14 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-    if (digitalRead(7))
+//    if (digitalRead(7)== LOW)
+    if (!digitalRead(7))
         {
-              digitalWrite(13, HIGH);    // turn the LED off by making the voltage LOW
+              digitalWrite(13, HIGH);    
         }
         else
         {
-              digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+              digitalWrite(13, LOW);   
         }
         
 }
--- 105台大創新原型實作/Code/WiFiUdpNtpClient/WiFiUdpNtpClient.ino
+++ 105台大創新原型實作/Code/WiFiUdpNtpClient/WiFiUdpNtpClient.ino
@@ -1,27 +1,9 @@
-/*
-
- Udp NTP Client
-
- Get the time from a Network Time Protocol (NTP) time server
- Demonstrates use of UDP sendPacket and ReceivePacket
- For more on NTP time servers and the messages needed to communicate with them,
- see http://en.wikipedia.org/wiki/Network_Time_Protocol
-
- created 4 Sep 2010
- by Michael Margolis
- modified 9 Apr 2012
- by Tom Igoe
-
- This code is in the public domain.
-
- */
-
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-int status = WL_IDLE_STATUS;
-char ssid[] = "mynetwork";  //  your network SSID (name)
-char pass[] = "mypassword";       // your network password
+uint8_t MacData[6];
+char ssid[] = "PM25";      // your network SSID (name)
+char pass[] = "qq12345678";     // your network password
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
 unsigned int localPort = 2390;      // local port to listen for UDP packets
@@ -34,8 +16,12 @@ byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing pack
 
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP Udp;
+IPAddress  Meip ,Megateway ,Mesubnet ;
+String MacAddress ;
+int status = WL_IDLE_STATUS;
 
-void setup() {
+void setup() 
+{
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -43,32 +29,29 @@ void setup() {
   }
 
   // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
+  if (WiFi.status() == WL_NO_SHIELD) 
+  {
     Serial.println("WiFi shield not present");
     // don't continue:
     while (true);
   }
 
   String fv = WiFi.firmwareVersion();
-  if (fv != "1.1.0") {
+  if (fv != "1.1.0") 
+  {
     Serial.println("Please upgrade the firmware");
   }
 
+    MacAddress = GetWifiMac() ; // get MacAddress
+    ShowMac() ;       //Show Mac Address
+    
   // attempt to connect to Wifi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-
+    initializeWiFi();
+ 
   Serial.println("Connected to wifi");
-  printWifiStatus();
+      ShowInternetStatus();
 
-  Serial.println("\nStarting connection to server...");
+  Serial.println("Starting connection to server...");
   Udp.begin(localPort);
 }
 
@@ -154,22 +137,140 @@ unsigned long sendNTPpacket(IPAddress& address) {
 }
 
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+void ShowMac()
+{
+  
+     Serial.print("MAC:");
+     Serial.print(MacAddress);
+     Serial.print("\n");
 
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
 }
+
+
+
+
+String GetWifiMac()
+{
+   String tt ;
+    String t1,t2,t3,t4,t5,t6 ;
+    WiFi.status();    //this method must be used for get MAC
+  WiFi.macAddress(MacData);
+  
+  Serial.print("Mac:");
+   Serial.print(MacData[0],HEX) ;
+   Serial.print("/");
+   Serial.print(MacData[1],HEX) ;
+   Serial.print("/");
+   Serial.print(MacData[2],HEX) ;
+   Serial.print("/");
+   Serial.print(MacData[3],HEX) ;
+   Serial.print("/");
+   Serial.print(MacData[4],HEX) ;
+   Serial.print("/");
+   Serial.print(MacData[5],HEX) ;
+   Serial.print("~");
+   
+   t1 = print2HEX((int)MacData[0]);
+   t2 = print2HEX((int)MacData[1]);
+   t3 = print2HEX((int)MacData[2]);
+   t4 = print2HEX((int)MacData[3]);
+   t5 = print2HEX((int)MacData[4]);
+   t6 = print2HEX((int)MacData[5]);
+ tt = (t1+t2+t3+t4+t5+t6) ;
+Serial.print(tt);
+Serial.print("\n");
+  
+  return tt ;
+}
+String  print2HEX(int number) {
+  String ttt ;
+  if (number >= 0 && number < 16)
+  {
+    ttt = String("0") + String(number,HEX);
+  }
+  else
+  {
+      ttt = String(number,HEX);
+  }
+  return ttt ;
+}
+
+
+
+
+
+void printWifiData() 
+{
+  // print your WiFi shield's IP address:
+  Meip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(Meip);
+  Serial.print("\n");
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+
+  // print your subnet mask:
+  Mesubnet = WiFi.subnetMask();
+  Serial.print("NetMask: ");
+  Serial.println(Mesubnet);
+
+  // print your gateway address:
+  Megateway = WiFi.gatewayIP();
+  Serial.print("Gateway: ");
+  Serial.println(Megateway);
+}
+
+void ShowInternetStatus()
+{
+    
+        if (WiFi.status())
+          {
+               Meip = WiFi.localIP();
+               Serial.print("Get IP is:");
+               Serial.print(Meip);
+               Serial.print("\n");
+              
+          }
+          else
+          {
+                       Serial.print("DisConnected:");
+                       Serial.print("\n");
+          }
+
+}
+
+void initializeWiFi() {
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+  //   status = WiFi.begin(ssid);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.print("\n Success to connect AP:") ;
+  Serial.print(ssid) ;
+  Serial.print("\n") ;
+  
+}
+
+
 
 
 
